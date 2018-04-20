fffa9809307934fef3e33d87b9341fe7a24822ea
Weiwei Jiang <weiweij@fb.com>
2018-04-20 23:54:19 +0100

    Initial Commit

---
+++ FB-SDK-MP/FB_SDK_MP.h
@@ -0,0 +1,19 @@
+//
+//  FB_SDK_MP.h
+//  FB-SDK-MP
+//
+//  Created by Weiwei Jiang on 2018/4/20.
+//  Copyright © 2018 Weiwei Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FB_SDK_MP.
+FOUNDATION_EXPORT double FB_SDK_MPVersionNumber;
+
+//! Project version string for FB_SDK_MP.
+FOUNDATION_EXPORT const unsigned char FB_SDK_MPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FB_SDK_MP/PublicHeader.h>
+
+

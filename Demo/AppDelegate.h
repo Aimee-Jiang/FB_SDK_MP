3300061eb1b1161245c70a07ba99e939267d0735
Weiwei Jiang <weiweij@fb.com>
2018-04-21 20:37:02 +0100

    first commit

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by Weiwei Jiang on 2018/4/21.
+//  Copyright © 2018 Weiwei Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Example
+//
+//  Created by Weiwei Jiang on 2018/4/21.
+//  Copyright © 2018 Weiwei Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
